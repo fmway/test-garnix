02961191172c3438c9e73e4dac8be4c2e46f70ca
fmway <fm18lv@gmail.com>
2024-10-19 21:45:29 +0700

    ...

--- flake-module.nix
+++ flake-module.nix
@@ -5,6 +5,17 @@
       system = "x86_64-linux";
       modules = [
         inputs.garnix-lib.nixosModules.garnix
+        {
+          _module.args.inputs.self = self;
+        }
+        {
+          garnix.server.enable = true;
+          nixpkgs.hostPlatform = "x86_64-linux";
+          services.nginx.enable = true;
+          security.sudo.wheelNeedsPassword = false;
+          services.openssh.enable = true;
+          networking.firewall.allowedTCPPorts = [ 80 ];
+        }
       ];
     };
   };
